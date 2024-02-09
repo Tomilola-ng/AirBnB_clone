609583470c8525953b988d8f0d0ae87b18dd3746
Tomilola-ng <tee.o2809@gmail.com>
2024-02-09 23:10:35 +0100

    CONSOLE_CONFIG

--- console.py
+++ console.py
@@ -7,8 +7,12 @@ class MyCommand(cmd.Cmd):
     """ Begin Console CLI Function """
     def do_EOF(self, line_arg):
         """ Gracefully Ends The Program """
-        print("")
+        print()
         return True
+
+    def default(self, line_arg):
+        """ Gracefully Catch Errors """
+        print(f'Invalid Command: {line_arg}')
 
 # ALLOWs THE FUNC() TO RUN.
 if __name__ == "__main__":
---
+++ tests/test_console.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+""" ALL TESTSCASES for console.py """
+
+

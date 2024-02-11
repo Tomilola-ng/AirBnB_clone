cc24a53ef1fb40c526143e018aa56d74b2018cc1
Tomilola-ng <tee.o2809@gmail.com>
2024-02-11 19:50:39 +0100

    HELP_SYNTAXES

--- console.py
+++ console.py
@@ -6,19 +6,18 @@ import cmd
 class MyCommand(cmd.Cmd):
     """ Begin Console CLI Function """
 
+    def do_sayHello(self, line):
+        """ Say Basicly Hello """
+        print("Hello there :)")
+
     def do_EOF(self, line):
         """ Gracefully Ends The Program """
         print()
         return True
-    
-    def do_sayHello(self, line):
-        """ Gracefully Ends The Program """
-        print("Hello there :)")
 
-    # def do_EOF(self, line_arg):
-    #     """ Gracefully Ends The Program """
-    #     print()
-    #     return True
+    def emptyline(self):
+        """ Ignore this request, when no arguments """
+        pass
 
     def default(self, line):
         """ Gracefully Catch Errors """
